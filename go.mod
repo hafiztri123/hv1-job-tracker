2e52a36c21411f06267912c2e917e32c9d9932bc
astra <hafiz.triwahyu@gmail.com>
2025-10-11 19:49:47 +0700

    feat: implement godotenv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hafiztri123/hv1-job-tracker
 
 go 1.23.5
+
+require github.com/joho/godotenv v1.5.1
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,4 +1,4 @@
-create table users (
+create table if not exists users (
     id uuid primary key default gen_random_uuid(),
     email varchar(255) unique not null,
     first_name varchar(50),
@@ -9,8 +9,8 @@ create table users (
     deleted_at timestamptz
 );
 
-create unique index idx_users_email_lower on users(lower(email)) where deleted_at is null;
+create unique index if not exists idx_users_email_lower on users(lower(email)) where deleted_at is null;
 
 
-create index idx_users_deleted_at on users(deleted_at) where deleted_at is null;
+create index if not exists idx_users_deleted_at on users(deleted_at) where deleted_at is null;
 
