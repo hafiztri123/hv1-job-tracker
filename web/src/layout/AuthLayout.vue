d28dd9f40b86ee4cdcb419524c9156e63499dfb4
Hafizh Tri <programmer-fe2@qwertysystem.net>
2025-10-14 15:33:48 +0700

    feat: add initial setup to make auth

---
+++ web/src/layout/AuthLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Auth from '@/components/module/auth/Auth.vue'
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-black/35 p-6">
+    <Auth />
+  </div>
+</template>
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,8 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import type { Component } from 'vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: Readonly<RouteRecordRaw[]> = [
+  {
+    path: '/',
+    name: 'auth',
+    component: (): Promise<Component> => import('@/layout/AuthLayout.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
